f0ff77734f8b13e76fb560f00eccde0ebf6d8ebb
Niclas Wiegleb <niclas.fenton-wiegleb@niclaswieglebhotmail.onmicrosoft.com>
2026-06-13 00:14:17 +0000

    Resolving app launch error due to CSS argument.

--- app.py
+++ app.py
@@ -1346,5 +1346,4 @@ if __name__ == "__main__":
     demo.queue().launch(
         server_name="0.0.0.0",
         server_port=int(os.getenv("GRADIO_SERVER_PORT", "7860")),
-        css=APP_CSS,
     )
